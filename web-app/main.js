4f1a4cc22c1144696394d58f16f268e660155a1a
HarveyDesEng <hg722@ic.ac.uk>
2023-06-14 18:38:26 +0100

    Made incomplete grid of dots, lines and boxes with scaling issues

--- web-app/main.js
+++ web-app/main.js
@@ -8,10 +8,21 @@ document.documentElement.style.setProperty("--game-columns", game_columns);
 
 const game_grid = document.getElementById("game_grid");
 
-R.range(0, game_rows).forEach(function (row_index) {
+R.range(0, (game_rows)).forEach(function (row_index) {
     R.range(0, (game_columns)).forEach(function (column_index) {
-        const td = document.createElement("td");
-        td.textContent = `${column_index},${row_index}`;
-        game_grid.append(td);
+        const dot = document.createElement("dot");
+        dot.textContent = `${column_index},${row_index}`;
+        game_grid.append(dot);
+        const h_line = document.createElement("h_line");
+        h_line.textContent = `${column_index},${row_index}`;
+        game_grid.append(h_line);
+    });
+    R.range(0, (game_columns)).forEach(function (column_index) {
+        const v_line = document.createElement("v_line");
+        v_line.textContent = `${column_index},${row_index}`;
+        game_grid.append(v_line);
+        const box = document.createElement("box");
+        box.textContent = `${column_index},${row_index}`;
+        game_grid.append(box);
     });
 });
