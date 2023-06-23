bfad349438363f94445dd315abd70efe29b28b37
HarveyDesEng <hg722@ic.ac.uk>
2023-06-23 12:27:13 +0100

    Jsdoc and comments are complete

--- web-app/DotsNBoxes.js
+++ web-app/DotsNBoxes.js
@@ -6,18 +6,6 @@ import R from "./ramda.js";
  */
 const DotsNBoxes = {};
 
-/**
- * Defining the Board type.
- * @typedef Board
- * @memberof DotsNBoxes
- */
-
-/**
- * Defining the Player type.
- * @typedef {(1 | 2)} Player
- * @memberof DotsNBoxes
- */
-
 
 /**
  * Function that creates boards of chosen widh and height.
@@ -28,17 +16,26 @@ const DotsNBoxes = {};
  * user.
  */
 
-const width = 5;
-const height = 4;
-
 const empty_board = function (width, height) {
     return R.repeat(R.repeat(0, width), height);
 };
+
+const width = 5;
+const height = 4;
 
 const empty_v_board = empty_board(width+1, height);
 const empty_h_board = empty_board(width, height+1);
 const empty_b_board = empty_board(width, height);
 
+
+/**
+ * Function that creates the starting state of the board which includes acts as
+ * an object holding teh vertical, horizontal and box boards as well as a
+ * counter for the moves made.
+ * @function
+ * @returns {DotsNBoxes.state} An Object holding the 3 boards and a counter for
+ * the moves made.
+ */
 DotsNBoxes.starting_state = function () {
     return {
         "v_board": empty_v_board,
@@ -48,10 +45,64 @@ DotsNBoxes.starting_state = function () {
     };
 };
 
-/* const needs_lighting = function (column_index, row_index, row_grid, column_grid) {
-    row_grid === column_index && column_grid ===row_index;
-};
+let add_moves = 1;
+
+/**
+ * Function that allows the player to make a ply from a vertical line.
+ * @function
+ * @param {DotsNBoxes.row_index} row_index The x index of line.
+ * @param {DotsNBoxes.column_index} column_index The y axis of the line.
+ * @returns {DotsNBoxes.game_state} A collection of all boards and moves made.
  */
+DotsNBoxes.v_ply = function (row_index, column_index,
+    game_state) {
+    if (game_state.v_board[row_index][column_index] === 1) {
+        return {
+            "v_board": game_state.v_board,
+            "h_board": game_state.h_board,
+            "b_board": game_state.b_board,
+            "moves_made": game_state.moves_made
+        };
+    } else {
+        return {
+            "v_board": ply_line_on_board(row_index, column_index,
+                game_state.v_board),
+            "h_board": game_state.h_board,
+            "b_board": DotsNBoxes.update_box_array_after_v(row_index,
+                column_index, game_state),
+            "moves_made": game_state.moves_made + add_moves
+        };
+    }
+};
+
+/**
+ * Function that allows the player to make a ply from a horizontal line line.
+ * @function
+ * @param {DotsNBoxes.row_index} row_index The x index of line.
+ * @param {DotsNBoxes.column_index} column_index The y axis of the line.
+ * @returns {DotsNBoxes.game_state} A collection of all boards and moves made.
+ */
+DotsNBoxes.h_ply = function (row_index, column_index,
+    game_state) {
+    if (game_state.h_board[row_index][column_index] === 1) {
+        return {
+            "v_board": game_state.v_board,
+            "h_board": game_state.h_board,
+            "b_board": game_state.b_board,
+            "moves_made": game_state.moves_made
+        }
+    } else {
+        return {
+            "v_board": game_state.v_board,
+            "h_board": ply_line_on_board(row_index, column_index,
+                game_state.h_board),
+            "b_board": DotsNBoxes.update_box_array_after_h(row_index,
+                column_index, game_state),
+            "moves_made": game_state.moves_made + add_moves
+        };
+    }
+};
+
 const player_to_ply = function (state) {
     const moves_made = state.moves_made;
     if (moves_made % 2 === 1) {
@@ -76,7 +127,7 @@ const ply_line_on_board = function (row_index, column_index, board) {
 };
 
 
-const ply_box_on_board = function (player, row_index, column_index, board, state) {
+const ply_box_on_board = function (player, row_index, column_index, board) {
     return (R.update(
         row_index,
         R.update(column_index, player, board[row_index]),
@@ -84,68 +135,14 @@ const ply_box_on_board = function (player, row_index, column_index, board, state
     ))
 };
 
-let add_moves = 1;
-
 /**
- * Function that allows the player to make a ply.
+ * Function that determines whether a box is made by the player after their ply
+ * on a vertical board and then returns a new state.
  * @function
- * @param {DotsNBoxes.Player} player The player making a ply.
- * @param {DotsNBoxes.Line_type} line_type If the line is horizontal or
- * verticle.
  * @param {DotsNBoxes.row_index} row_index The x index of line.
  * @param {DotsNBoxes.column_index} column_index The y axis of the line.
- * @param {DotsNBoxes.Board_of_Horizontal_Lines} board_h_lines The board of
-  horizontal lines before a move is made.
- * @param {DotsNBoxes.Board_of_Vertical_Lines} board_v_lines The board of
- * vertical lines before a move is made.
- * @returns {DotsNBoxes.game_state} A collection of all boards.
+ * @param {DotsNBoxes.Board} state A collection of all boards and moves made.
  */
-
-DotsNBoxes.v_ply = function (row_index, column_index,
-    game_state) {
-    if (game_state.v_board[row_index][column_index] === 1) {
-        return {
-            "v_board": game_state.v_board,
-            "h_board": game_state.h_board,
-            "b_board": game_state.b_board,
-            "moves_made": game_state.moves_made
-        };
-    } else {
-        return {
-            "v_board": ply_line_on_board(row_index, column_index, game_state.v_board),
-            "h_board": game_state.h_board,
-            "b_board": DotsNBoxes.update_box_array_after_v(row_index, column_index, game_state),
-            "moves_made": game_state.moves_made + add_moves
-        };
-    }
-};
-
-DotsNBoxes.h_ply = function (row_index, column_index,
-    game_state) {
-    if (game_state.h_board[row_index][column_index] === 1) {
-        return {
-            "v_board": game_state.v_board,
-            "h_board": game_state.h_board,
-            "b_board": game_state.b_board,
-            "moves_made": game_state.moves_made
-        }
-    } else {
-        return {
-            "v_board": game_state.v_board,
-            "h_board": ply_line_on_board(row_index, column_index, game_state.h_board),
-            "b_board": DotsNBoxes.update_box_array_after_h(row_index, column_index, game_state),
-            "moves_made": game_state.moves_made + add_moves
-        };
-    }
-};
-
-/**
- * Function that determines whether a box is made by the player after their ply.
- * @function
- * @param {DotsNBoxes.Player} player The player making a ply.
- * @param {DotsNBoxes.Board} board The board after the player's move.
- */
-
 DotsNBoxes.update_box_array_after_v = function (r_i, c_i, state) {
     if (
         //both boxes around vertical line
@@ -157,10 +154,10 @@ DotsNBoxes.update_box_array_after_v = function (r_i, c_i, state) {
         && (state.h_board[r_i+1][c_i-1] === 1)
     ) {
         const intermediate_board = ply_box_on_board(player_to_ply(state), r_i,
-        c_i, state.b_board, state);
+        c_i, state.b_board);
         add_moves = 0;
         return ply_box_on_board(player_to_ply(state), r_i, c_i-1,
-        intermediate_board, state);
+        intermediate_board);
     }
     if (
         // box to the right of vertical line
@@ -169,8 +166,7 @@ DotsNBoxes.update_box_array_after_v = function (r_i, c_i, state) {
         && (state.h_board[r_i+1][c_i] === 1)
     ) {
         add_moves = 0;
-        return ply_box_on_board(player_to_ply(state), r_i, c_i, state.b_board,
-        state);
+        return ply_box_on_board(player_to_ply(state), r_i, c_i, state.b_board);
     }
     if (
         // box to the left of vertical line
@@ -179,14 +175,21 @@ DotsNBoxes.update_box_array_after_v = function (r_i, c_i, state) {
         && (state.h_board[r_i+1][c_i-1] === 1)
     ) {
         add_moves = 0;
-        return ply_box_on_board(player_to_ply(state), r_i, c_i-1, state.b_board,
-        state);
+        return ply_box_on_board(player_to_ply(state), r_i, c_i-1, state.b_board);
     } else {
         add_moves = 1;
         return state.b_board;
     }
 };
 
+/**
+ * Function that determines whether a box is made by the player after their ply
+ * on a horizontal board and then returns a new state.
+ * @function
+ * @param {DotsNBoxes.row_index} row_index The x index of line.
+ * @param {DotsNBoxes.column_index} column_index The y axis of the line.
+ * @param {DotsNBoxes.Board} state A collection of all boards and moves made.
+ */
 DotsNBoxes.update_box_array_after_h = function (r_i, c_i, state) {
     if (r_i === 0) {
         if (
@@ -197,7 +200,7 @@ DotsNBoxes.update_box_array_after_h = function (r_i, c_i, state) {
         ) {
             add_moves = 0;
             return ply_box_on_board(player_to_ply(state), r_i, c_i,
-            state.b_board, state);
+            state.b_board);
         } else {
             add_moves = 1;
             return state.b_board;
@@ -212,7 +215,7 @@ DotsNBoxes.update_box_array_after_h = function (r_i, c_i, state) {
         ) {
             add_moves = 0;;
             return ply_box_on_board(player_to_ply(state), r_i-1, c_i,
-            state.b_board, state);
+            state.b_board);
         } else {
             add_moves = 1;
             return state.b_board;
@@ -228,10 +231,10 @@ DotsNBoxes.update_box_array_after_h = function (r_i, c_i, state) {
         && (state.v_board[r_i][c_i+1] === 1)
     ) {
         const intermediate_board = ply_box_on_board(player_to_ply(state), r_i-1,
-        c_i, state.b_board, state);
+        c_i, state.b_board);
         add_moves = 0;
         return ply_box_on_board(player_to_ply(state), r_i, c_i,
-        intermediate_board, state);
+        intermediate_board);
     }
     if (
         // box above horizontal line
@@ -241,7 +244,7 @@ DotsNBoxes.update_box_array_after_h = function (r_i, c_i, state) {
     ) {
         add_moves = 0;
         return ply_box_on_board(player_to_ply(state), r_i-1, c_i,
-        state.b_board, state);
+        state.b_board);
     }
     if (
         // box below horizonal line
@@ -250,43 +253,17 @@ DotsNBoxes.update_box_array_after_h = function (r_i, c_i, state) {
         && (state.v_board[r_i][c_i+1] === 1)
     ) {
         add_moves = 0;
-        return ply_box_on_board(player_to_ply(state), r_i, c_i, state.b_board,
-        state);
+        return ply_box_on_board(player_to_ply(state), r_i, c_i, state.b_board);
     } else {
         add_moves = 1;
         return state.b_board;
     }
 };
-
 /**
- * Function that determines the places where a move can be made by the player.
- * @function
- * @param {DotsNBoxes.Board} board The board before a player makes a move.
- */
-DotsNBoxes.free_moves = function (board) {
-};
-
-/**
- * Function that determines whether the game is tied afte a player ply.
- * @function
- * @param {DotsNBoxes.Board} board The board after a player has moved.
- */
-DotsNBoxes.is_game_tied = function (board) {
-};
-
-/**
- * Function that determines whether a player has won after a player ply.
- * @function
- * @param {DotsNBoxes.Player} player The player that just moved.
- * @param {DotsNBoxes.Player} board The board after the player has moved.
- */
-DotsNBoxes.is_winning_for_player = function (player, board) {
-};
-
-/**
-* Returns a  function, mapping tokens to provided string representations.
+* Returns a  function, mapping a board to provided string representations.
 * @function
-* @param {string[]} thing_strings
+* @param {DotsNBoxes.board} board The board being converted into a string
+* representation.
 * @returns {function} The string representation.
 */
 DotsNBoxes.to_string = (board) => R.pipe(
@@ -296,6 +273,13 @@ DotsNBoxes.to_string = (board) => R.pipe(
     R.join("\n") // Stack rows atop each other.
 )(board);
 
+/**
+ * Function that determines whether a player has won after their ply
+ * on a horizontal board and then returns a number representing the results.
+ * @function
+ * @param {DotsNBoxes.Board} state A collection of all boards and moves made.
+ * @returns {Number} The number reperesenting the results of the game.
+ */
 DotsNBoxes.player_has_won = function (state) {
     if (
         R.count(R.equals(1), R.flatten(state.v_board)) === (width+1)*(height)
--- web-app/main.js
+++ web-app/main.js
@@ -1,6 +1,7 @@
 import DotsNBoxes from "./DotsNBoxes.js";
 import R from "./ramda.js";
 
+// Array of result texts.
 const result_text = [
     "Draw!",
     "Red wins!",
@@ -10,16 +11,20 @@ const result_text = [
 const result_dialog = document.getElementById("result_dialog");
 const game_result = document.getElementById("game_result");
 
+// Variables defining the amount of rows and columns.
 const game_rows = 4;
 const game_columns = 5;
 
+// The first assigning of the game state.
 let game_state = DotsNBoxes.starting_state();
 
 document.documentElement.style.setProperty("--game-rows", game_rows);
 document.documentElement.style.setProperty("--game-columns", game_columns);
 
+// Assigning the game_grid from index.html a variable.
 const game_grid = document.getElementById("game_grid");
 
+// Function that make a vertical line element.
 const make_v_line = function (r_i, c_i) {
     const v_line = document.createElement("v_line");
     v_line.textContent = `${r_i},${c_i}`;
@@ -40,6 +45,7 @@ const make_v_line = function (r_i, c_i) {
     return v_line;
 };
 
+// Function that make a horizontal line element.
 const make_h_line = function (r_i, c_i) {
     const h_line = document.createElement("h_line");
     h_line.textContent = `${r_i},${c_i}`;
@@ -60,17 +66,20 @@ const make_h_line = function (r_i, c_i) {
     return h_line;
 };
 
+// Function that make a box element.
 const make_box = function (r_i, c_i) {
     const box = document.createElement(`box`);
     box.textContent = `${r_i},${c_i}`;
     game_grid.append(box);
 };
 
+// Function that make a dot element.
 const make_dot = function () {
     const dot = document.createElement(`dot`);
     game_grid.append(dot);
 };
 
+// The assembly of the game grid to be played on.
 R.range(0, (game_rows)).forEach(function (row_index) {
     R.range(0, (game_columns)).forEach(function (column_index) {
         make_dot();
@@ -89,22 +98,28 @@ R.range(0, (game_columns)).forEach(function (column_index) {
 });
 make_dot();
 
+// Collecting the horizontal line elements into an array.
 const table_h_lines = (
     Array.from(document.getElementsByTagName("h_line"))
 );
 
+// Collecting the vertical line elements into an array.
 const table_v_lines = (
     Array.from(document.getElementsByTagName("v_line"))
 );
 
+// Collecting the box elements into an array.
 const table_boxes = (
     Array.from(document.getElementsByTagName("box"))
 );
 
+// Function that updates the game_grid from the v_board inside the game_state.
 const update_v_board = function () {
     game_state.v_board.forEach(function (row, row_index) {
         row.forEach(function (cell, column_index) {
-            const table_v_line = table_v_lines[(row_index) * (game_columns + 1) + column_index];
+            const table_v_line = (
+                table_v_lines[(row_index) * (game_columns + 1) + column_index]
+            );
             table_v_line.className = (
                 (cell === 1)
                 ? "lit"
@@ -114,10 +129,13 @@ const update_v_board = function () {
     });
 };
 
+// Function that updates the game_grid from the h_board inside the game_state.
 const update_h_board = function () {
     game_state.h_board.forEach(function (row, row_index) {
         row.forEach(function (cell, column_index) {
-            const table_h_line = table_h_lines[(row_index) * (game_columns) + column_index];
+            const table_h_line = (
+                table_h_lines[(row_index) * (game_columns) + column_index]
+            );
             table_h_line.className = (
                 (cell === 1)
                 ? "lit"
@@ -127,12 +145,15 @@ const update_h_board = function () {
     });
 };
 
+// Function that updates the game_grid from the b_board in the game_state.
 const update_b_board = function () {
     console.log(game_state.b_board);
     console.log(table_boxes);
     game_state.b_board.forEach(function (row, row_index) {
         row.forEach(function (cell, column_index) {
-            const table_box = table_boxes[(row_index) * (game_columns) + column_index];
+            const table_box = (
+                table_boxes[(row_index) * (game_columns) + column_index]
+            );
             if (cell === 1) {
                 return table_box.className = "red";
             }
